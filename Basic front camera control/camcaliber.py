e7c4876d22953b1e70edf3049fe16516e48af19e
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-05-27 09:49:21 +0530

    Add files via upload

---
+++ Basic front camera control/camcaliber.py
@@ -0,0 +1,70 @@
+#This file is for the calibration of the camera.
+
+#importing libraries
+import numpy as np
+import cv2
+import glob
+
+#delaring the square size:
+sq_size = 10.22/400 #in meter
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+objp = objp * sq_size
+
+#now we must create the arrays store objp and imgp (object points and image point) from all images
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('calibration_*.jpg')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (6,9),None)
+    print('ret=',ret)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (6,9), corners2,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+
+cv2.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+print('Camera Matrix', mtx)
+
+img = cv2.imread('calibration_33.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0.1,(w,h))
+
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+# crop the image
+#x,y,w,h = roi
+#dst = dst[y:y+h, x:x+w]
+#cv2.imshow('img',img)
+#cv2.imshow('undistorted',dst)
+#cv2.waitKey(5000)
+
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    mean_error += error
+
+print("total error: ", mean_error/len(objpoints))
+
